d6cf67dc9258d05b15bc71f731d09c165f3f9f98
HamedGh <h.ghadirian@outlook.com>
2016-04-29 14:19:10 +0430

    modify MainActivity

--- app/src/main/java/hamed_gh/ir/divaaremehrabani/MainActivity.java
+++ app/src/main/java/hamed_gh/ir/divaaremehrabani/MainActivity.java
@@ -2,7 +2,6 @@ package hamed_gh.ir.divaaremehrabani;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		final TextView textView = (TextView)findViewById(R.id.textview);
 	}
 }
