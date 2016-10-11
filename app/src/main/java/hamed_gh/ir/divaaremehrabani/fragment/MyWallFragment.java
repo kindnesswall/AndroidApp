574e9fa21ad9b14eaba875c184bac7bde83e732f
HamedGh <h.ghadirian@outlook.com>
2016-10-11 23:32:45 +0330

    add bookmark and location to the MyWallFragment

--- app/src/main/java/hamed_gh/ir/divaaremehrabani/fragment/MyWallFragment.java
+++ app/src/main/java/hamed_gh/ir/divaaremehrabani/fragment/MyWallFragment.java
@@ -1,22 +1,34 @@
 package hamed_gh.ir.divaaremehrabani.fragment;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import hamed_gh.ir.divaaremehrabani.R;
+import hamed_gh.ir.divaaremehrabani.app.AppController;
 
 /**
  * Created by 5 on 02/21/2016.
  */
 public class MyWallFragment extends BaseFragment {
 
-	@Bind(R.id.message_textview)
+	@Bind(R.id.my_gift_txt)
 	TextView mMessageTextView;
+
+	@Bind(R.id.my_gift_iv)
+	ImageView mMyGiftIv;
+
+	@Bind(R.id.bookmark_iv)
+	ImageView mBookmarIv;
+
+	@Bind(R.id.location_iv)
+	ImageView mLocationIv;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +38,11 @@ public class MyWallFragment extends BaseFragment {
 
 		ButterKnife.bind(this, rootView);
 		init();
+
+		int color = AppController.getAppContext().getResources().getColor(R.color.light_black);
+		mMyGiftIv.setColorFilter( color, PorterDuff.Mode.SRC_ATOP);
+		mBookmarIv.setColorFilter( color, PorterDuff.Mode.SRC_ATOP);
+		mLocationIv.setColorFilter( color, PorterDuff.Mode.SRC_ATOP);
 
 		return rootView;
 	}
