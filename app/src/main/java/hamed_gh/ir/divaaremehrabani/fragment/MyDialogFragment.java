626e6893e08254a85bcc4753d7b7d109a644215f
HamedGh <h.ghadirian@outlook.com>
2016-10-12 11:59:03 +0330

    redesign choose city dialog

--- app/src/main/java/hamed_gh/ir/divaaremehrabani/fragment/MyDialogFragment.java
+++ app/src/main/java/hamed_gh/ir/divaaremehrabani/fragment/MyDialogFragment.java
@@ -1,6 +1,5 @@
 package hamed_gh.ir.divaaremehrabani.fragment;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,15 +11,12 @@ import android.view.ViewGroup;
 import butterknife.ButterKnife;
 import hamed_gh.ir.divaaremehrabani.R;
 import hamed_gh.ir.divaaremehrabani.adapter.ChooseCityAdapter;
-import hamed_gh.ir.divaaremehrabani.app.AppController;
 import hamed_gh.ir.divaaremehrabani.customviews.edit_text.EditTextIranSans;
 
 /**
  * Created by 5 on 02/21/2016.
  */
 public class MyDialogFragment extends DialogFragment {
-
-
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,9 +28,8 @@ public class MyDialogFragment extends DialogFragment {
 
 		RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.choose_city_recyclerview);
 		EditTextIranSans editTextIranSans = (EditTextIranSans) rootView.findViewById(R.id.choose_city_et);
-
-		int color = AppController.getAppContext().getResources().getColor(R.color.colorAccent);
-		editTextIranSans.getCompoundDrawables()[2].setColorFilter( color, PorterDuff.Mode.SRC_ATOP);
+//		int color = AppController.getAppContext().getResources().getColor(R.color.colorAccent);
+//		editTextIranSans.getCompoundDrawables()[2].setColorFilter( color, PorterDuff.Mode.SRC_ATOP);
 
 		ChooseCityAdapter chooseCityAdapter = new ChooseCityAdapter(getContext());
 		recyclerView.setAdapter(chooseCityAdapter);
