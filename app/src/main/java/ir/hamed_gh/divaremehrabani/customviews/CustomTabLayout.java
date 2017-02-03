178ce6902e94253a818da6a8f89c4194d289b54e
Hamed.Gh <h.ghadirian@outlook.com>
2017-02-03 10:46:53 +0330

    1-Create ViewPagerAdapter 2-Add CustomTabLayout 3-Create RegisteredGiftsFragment

---
+++ app/src/main/java/ir/hamed_gh/divaremehrabani/customviews/CustomTabLayout.java
@@ -0,0 +1,47 @@
+package ir.hamed_gh.divaremehrabani.customviews;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hamed on 4/13/16.
+ */
+public class CustomTabLayout extends TabLayout {
+    public CustomTabLayout(Context context) {
+        super(context);
+    }
+
+    public CustomTabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setupWithViewPager(ViewPager viewPager) {
+        super.setupWithViewPager(viewPager);
+        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/iran_sans_bold_second.ttf");
+        if (typeface != null) {
+            this.removeAllTabs();
+
+            ViewGroup slidingTabStrip = (ViewGroup) getChildAt(0);
+
+            PagerAdapter adapter = viewPager.getAdapter();
+
+            for (int i = 0, count = adapter.getCount(); i < count; i++) {
+                Tab tab = this.newTab();
+                this.addTab(tab.setText(adapter.getPageTitle(i)));
+                AppCompatTextView view = (AppCompatTextView) ((ViewGroup) slidingTabStrip.getChildAt(i)).getChildAt(1);
+                view.setTypeface(typeface, Typeface.NORMAL);
+            }
+        }
+    }
+}
--- app/src/main/java/ir/hamed_gh/divaremehrabani/fragment/MyRequestsFragment.java
+++ app/src/main/java/ir/hamed_gh/divaremehrabani/fragment/MyRequestsFragment.java
@@ -2,6 +2,7 @@ package ir.hamed_gh.divaremehrabani.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,10 @@ import butterknife.ButterKnife;
 import ir.hamed_gh.divaremehrabani.R;
 import ir.hamed_gh.divaremehrabani.activity.BottomBarActivity;
 import ir.hamed_gh.divaremehrabani.activity.LoginActivity;
+import ir.hamed_gh.divaremehrabani.adapter.ViewPagerAdapter;
 import ir.hamed_gh.divaremehrabani.app.AppController;
 import ir.hamed_gh.divaremehrabani.app.Constants;
+import ir.hamed_gh.divaremehrabani.customviews.CustomTabLayout;
 
 /**
  * Created by 5 on 02/21/2016.
@@ -26,18 +29,40 @@ public class MyRequestsFragment extends BaseFragment {
     @Bind(R.id.my_gift_top_lay)
     RelativeLayout myGiftTopLay;
 
+    @Bind(R.id.main_tabs)
+    CustomTabLayout mainTabs;
+
+    @Bind(R.id.main_vp)
+    ViewPager mainVp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_my_gifts, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_my_requests, container, false);
 
         ButterKnife.bind(this, rootView);
         init();
 
         ((BottomBarActivity) getActivity()).mToolbarTitleTextView.setText("هدیه‌های من");
 
+        setupViewPager(mainVp);
+        mainTabs.setupWithViewPager(mainVp);
+
         return rootView;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(mainActivity.getSupportFragmentManager());
+
+        RegisteredGiftsFragment registeredGiftsFragment = new RegisteredGiftsFragment();
+        RegisteredGiftsFragment registeredGiftsFragment2 = new RegisteredGiftsFragment();
+
+        adapter.addFrag(registeredGiftsFragment, "درخواستی");
+        adapter.addFrag(registeredGiftsFragment2, "ارسالی");
+
+        viewPager.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
