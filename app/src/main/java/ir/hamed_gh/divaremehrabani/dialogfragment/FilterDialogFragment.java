36ffbcdcddd726ceec073d1f2f35d835bd14a37a
Hamed.Gh <h.ghadirian@outlook.com>
2017-02-09 09:24:12 +0330

    set listeners in FilterDialogFragment

--- app/src/main/java/ir/hamed_gh/divaremehrabani/dialogfragment/FilterDialogFragment.java
+++ app/src/main/java/ir/hamed_gh/divaremehrabani/dialogfragment/FilterDialogFragment.java
@@ -5,15 +5,29 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import ir.hamed_gh.divaremehrabani.R;
+import ir.hamed_gh.divaremehrabani.customviews.textviews.TextViewIranSansRegular;
 
 /**
  * Created by 5 on 02/21/2016.
  */
 public class FilterDialogFragment extends DialogFragment {
 
+    @Bind(R.id.category_filter_lay)
+    RelativeLayout categoryFilterLay;
+
+    @Bind(R.id.location_filter_lay)
+    RelativeLayout locationFilterLay;
+
+    @Bind(R.id.apply_filter_tv)
+    TextViewIranSansRegular applyFilterTv;
+
+    @Bind(R.id.cancel_filter_tv)
+    TextViewIranSansRegular cancelFilterTv;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,12 +37,45 @@ public class FilterDialogFragment extends DialogFragment {
 
         ButterKnife.bind(this, rootView);
 
+        setListeners();
+
 //        ChooseCityAdapter chooseCityAdapter = new ChooseCityAdapter(getContext(), level2.getPlaces());
 //        recyclerView.setAdapter(chooseCityAdapter);
 //        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         return rootView;
     }
+
+    private void setListeners() {
+        categoryFilterLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        locationFilterLay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        applyFilterTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cancelFilterTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
 
     @Override
     public void onStart() {
