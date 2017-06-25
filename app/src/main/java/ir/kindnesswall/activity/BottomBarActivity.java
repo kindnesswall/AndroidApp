eb97b9a38db120269228d376eac2f5867c618d62
Hamed.Gh <h.ghadirian@outlook.com>
2017-06-25 23:56:49 +0430

    check app update in splash screen instead of BottomBarActivity

--- app/src/main/java/ir/kindnesswall/activity/BottomBarActivity.java
+++ app/src/main/java/ir/kindnesswall/activity/BottomBarActivity.java
@@ -1,8 +1,6 @@
 package ir.kindnesswall.activity;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
@@ -29,14 +27,9 @@ import ir.kindnesswall.fragment.mywall.StatisticFragment;
 import ir.kindnesswall.fragment.mywall.mygifts.MyGiftsFragment;
 import ir.kindnesswall.fragment.mywall.requests.MyRequestsFragment;
 import ir.kindnesswall.fragment.mywall.requests.RequestsToAGiftFragment;
-import ir.kindnesswall.helper.ApiRequest;
-import ir.kindnesswall.helper.DeviceInfo;
 import ir.kindnesswall.helper.UpdateChecker;
-import ir.kindnesswall.model.api.output.UpdateOutput;
-import retrofit2.Call;
-import retrofit2.Response;
 
-public class BottomBarActivity extends AppCompatActivity implements ApiRequest.Listener {
+public class BottomBarActivity extends AppCompatActivity{
 
 	@Bind(R.id.toolbar_title_textView)
 	public TextView mToolbarTitleTextView;
@@ -202,8 +195,7 @@ public class BottomBarActivity extends AppCompatActivity implements ApiRequest.L
 			}
 		});
 
-		ApiRequest apiRequest = new ApiRequest(this, this);
-		apiRequest.getUpdatedVersion();
+		setContent();
 	}
 
 	private void setContent() {
@@ -305,61 +297,6 @@ public class BottomBarActivity extends AppCompatActivity implements ApiRequest.L
 
 			}
 		}
-
-	}
-
-	private void onUpdateVersionResponse(UpdateOutput updateOutput) {
-
-		boolean isForcedUpdate;
-
-		if (updateOutput.force_update != null && updateOutput.force_update.equalsIgnoreCase("true")) {
-			isForcedUpdate = true;
-		} else {
-			isForcedUpdate = false;//todo use this
-		}
-
-		UpdateChecker updateChecker = new UpdateChecker(
-				getResources().getString(R.string.app_name),
-				updateOutput.version,
-				updateOutput.apk_url,
-				null,
-				updateOutput.changes);
-
-		if (!isForcedUpdate) {
-//					callApiGetHomeByTagId();
-			setContent();
-		}
-
-		if (DeviceInfo.getAppVersionCode() < Integer.valueOf(updateChecker.mUpdateDetail.latestVersion)) {
-			//Notify Update
-			Intent[] intents = new Intent[1];
-			intents[0] = Intent.makeMainActivity(new ComponentName(AppController.getAppContext(),
-					BottomBarActivity.class));
-			// intents[1] = new Intent(AppController.getAppContext(), HomeActivity.class);
-			updateChecker.showUpdaterDialog(
-					context,
-					getString(R.string.update_to_new_version),
-					getString(R.string.exist_new_version),
-					updateOutput.changes,
-					updateOutput.version,
-					intents,
-					isForcedUpdate);
-
-			AppController.getInstance().setIsCheckedUpdate(true);
-
-		}
-
-	}
-
-	@Override
-	public void onResponse(Call call, Response response) {
-		if (response.body() instanceof UpdateOutput) {
-			onUpdateVersionResponse((UpdateOutput) response.body());
-		}
-	}
-
-	@Override
-	public void onFailure(Call call, Throwable t) {
 
 	}
 }
--- app/src/main/java/ir/kindnesswall/activity/SplashScreenActivity.java
+++ app/src/main/java/ir/kindnesswall/activity/SplashScreenActivity.java
@@ -1,8 +1,9 @@
 package ir.kindnesswall.activity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,14 +11,20 @@ import ir.hamed_gh.kindnesswall.R;
 import ir.kindnesswall.app.AppController;
 import ir.kindnesswall.constants.Constants;
 import ir.kindnesswall.dialogfragment.ChoosePlaceDialogFragment;
+import ir.kindnesswall.helper.ApiRequest;
+import ir.kindnesswall.helper.DeviceInfo;
+import ir.kindnesswall.helper.UpdateChecker;
 import ir.kindnesswall.interfaces.ChoosePlaceCallback;
 import ir.kindnesswall.model.Place;
+import ir.kindnesswall.model.api.output.UpdateOutput;
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Created by HamedGh on 3/8/2016.
  */
 
-public class SplashScreenActivity extends AppCompatActivity implements ChoosePlaceCallback {
+public class SplashScreenActivity extends AppCompatActivity implements ChoosePlaceCallback,ApiRequest.Listener  {
 
 	/**
 	 * Duration of wait
@@ -27,23 +34,30 @@ public class SplashScreenActivity extends AppCompatActivity implements ChoosePla
 	/**
 	 * Called when the activity is first created.
 	 */
+
+	Context context;
+
 	@Override
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
 		setContentView(R.layout.activity_splash);
 
+		context = this;
+
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
 		if (AppController.getStoredString(Constants.MY_LOCATION_ID) != null) {
-			new Handler().postDelayed(new Runnable() {
-				@Override
-				public void run() {
-	            /* Create an Intent that will start the Menu-Activity. */
-					Intent mainIntent = new Intent(SplashScreenActivity.this, BottomBarActivity.class);
-					SplashScreenActivity.this.startActivity(mainIntent);
-					SplashScreenActivity.this.finish();
-				}
-			}, SPLASH_DISPLAY_LENGTH);
+
+			ApiRequest apiRequest = new ApiRequest(this, this);
+			apiRequest.getUpdatedVersion();
+
+//			new Handler().postDelayed(new Runnable() {
+//				@Override
+//				public void run() {
+//	            /* Create an Intent that will start the Menu-Activity. */
+//
+//				}
+//			}, SPLASH_DISPLAY_LENGTH);
 
 		} else {
 
@@ -71,6 +85,65 @@ public class SplashScreenActivity extends AppCompatActivity implements ChoosePla
 
 	@Override
 	public void onRegionSelected(Place region) {
+
+	}
+
+	private void onUpdateVersionResponse(UpdateOutput updateOutput) {
+
+		boolean isForcedUpdate;
+
+		if (updateOutput.force_update != null && updateOutput.force_update.equalsIgnoreCase("true")) {
+			isForcedUpdate = true;
+		} else {
+			isForcedUpdate = false;//todo use this
+		}
+
+		UpdateChecker updateChecker = new UpdateChecker(
+				getResources().getString(R.string.app_name),
+				updateOutput.version,
+				updateOutput.apk_url,
+				null,
+				updateOutput.changes);
+
+		if (!isForcedUpdate) {
+//					callApiGetHomeByTagId();
+
+			Intent mainIntent = new Intent(SplashScreenActivity.this, BottomBarActivity.class);
+			SplashScreenActivity.this.startActivity(mainIntent);
+			SplashScreenActivity.this.finish();
+
+		}
+
+		if (DeviceInfo.getAppVersionCode() < Integer.valueOf(updateChecker.mUpdateDetail.latestVersion)) {
+			//Notify Update
+			Intent[] intents = new Intent[1];
+			intents[0] = Intent.makeMainActivity(new ComponentName(AppController.getAppContext(),
+					BottomBarActivity.class));
+			// intents[1] = new Intent(AppController.getAppContext(), HomeActivity.class);
+			updateChecker.showUpdaterDialog(
+					context,
+					getString(R.string.update_to_new_version),
+					getString(R.string.exist_new_version),
+					updateOutput.changes,
+					updateOutput.version,
+					intents,
+					isForcedUpdate);
+
+			AppController.getInstance().setIsCheckedUpdate(true);
+
+		}
+
+	}
+
+	@Override
+	public void onResponse(Call call, Response response) {
+		if (response.body() instanceof UpdateOutput) {
+			onUpdateVersionResponse((UpdateOutput) response.body());
+		}
+	}
+
+	@Override
+	public void onFailure(Call call, Throwable t) {
 
 	}
 }
