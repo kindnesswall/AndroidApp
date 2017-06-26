c7b660f97c03114950a164bfb6371584d5eb567b
Hamed.Gh <h.ghadirian@outlook.com>
2017-06-26 17:35:11 +0430

    Add app intro

---
+++ app/src/main/java/ir/kindnesswall/activity/AppIntro.java
@@ -0,0 +1,78 @@
+package ir.kindnesswall.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+import ir.kindnesswall.R;
+
+public class AppIntro extends IntroActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_main);
+
+		/* Show/hide button */
+		setButtonBackVisible(false);
+/* Use skip button behavior */
+		setButtonBackFunction(BUTTON_BACK_FUNCTION_SKIP);
+/* Use back button behavior */
+//		setButtonBackFunction(BUTTON_BACK_FUNCTION_BACK);
+
+		/* Show/hide button */
+		setButtonNextVisible(false);
+/* Use next and finish button behavior */
+		setButtonNextFunction(BUTTON_NEXT_FUNCTION_NEXT_FINISH);
+/* Use next button behavior */
+//		setButtonNextFunction(BUTTON_NEXT_FUNCTION_NEXT);
+
+		setPageScrollDuration(1000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setPageScrollInterpolator(android.R.interpolator.fast_out_slow_in);
+        }
+
+		addSlide(new SimpleSlide.Builder()
+				.title("فرهنگ دیوار مهربانی")
+				.description("نیاز داری، بردار. نیاز نداری، بگذار :)")
+				.image(R.drawable.kindnesswall)
+				.background(R.color.colorPrimary)
+				.backgroundDark(R.color.colorAccent)
+				.layout(R.layout.activity_app_into)
+				.build());
+
+		addSlide(new SimpleSlide.Builder()
+				.title("مشابه برنامه دیوار")
+				.description("به دلیل تعداد زیاد کاربران برنامه دیوار، برای راحتی کاربرها ظاهر این برنامه مشابه برنامه دیوار طراحی شده است.")
+				.image(R.drawable.divar)
+				.background(R.color.colorPrimary)
+				.backgroundDark(R.color.colorAccent)
+				.layout(R.layout.activity_app_into)
+				.build());
+
+		addSlide(new SimpleSlide.Builder()
+				.title("همیشه رایگان")
+				.description("برنامه دیوار مهربانی برای همیشه رایگان خواهد ماند. بدون هر گونه تبلیغات.")
+				.image(R.drawable.gift)
+				.background(R.color.colorPrimary)
+				.backgroundDark(R.color.colorAccent)
+				.layout(R.layout.activity_app_into)
+				.build());
+
+		addSlide(new SimpleSlide.Builder()
+                .title("متن باز")
+                .description("کدهای برنامه همیشه در دسترس برنامه نویسان خواهد بود.")
+                .image(R.drawable.osi)
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorAccent)
+                .layout(R.layout.activity_app_into)
+                .build());
+
+//		setPageScrollDuration(500);
+		autoplay(1000, 1);
+
+
+	}
+}
