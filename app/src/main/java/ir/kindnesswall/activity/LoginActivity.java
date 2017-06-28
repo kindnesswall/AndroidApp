fe91b90bbf926d92854cb2b1ce40996f11d40b6d
Hamed.Gh <h.ghadirian@outlook.com>
2017-06-29 00:55:28 +0430

    receive and read confirmation code from sms inbox

--- app/src/main/java/ir/kindnesswall/activity/LoginActivity.java
+++ app/src/main/java/ir/kindnesswall/activity/LoginActivity.java
@@ -1,13 +1,17 @@
 package ir.kindnesswall.activity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -26,12 +30,13 @@ import ir.kindnesswall.helper.ApiRequest;
 import ir.kindnesswall.helper.DeviceInfo;
 import ir.kindnesswall.helper.NumberTranslator;
 import ir.kindnesswall.helper.Snackbari;
-import ir.kindnesswall.helper.Toasti;
 import ir.kindnesswall.model.api.input.SetDeviceInput;
 import ir.kindnesswall.model.api.output.RegisterOutput;
 import ir.kindnesswall.model.api.output.TokenOutput;
 import retrofit2.Call;
 import retrofit2.Response;
+
+import static ir.kindnesswall.helper.Toasti.showS;
 
 public class LoginActivity extends AppCompatActivity implements ApiRequest.Listener {
 
@@ -86,6 +91,24 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 		mToolbarTitleTextView.setText("ورود به دیوار مهربانی");
 	}
 
+	private void loginWithCode(String confirmationCode){
+
+		phoneConfirimationCodeEt.setText(confirmationCode);
+		if (!confirmationCode.trim().matches(regexStr)) {
+			Snackbari.showS(
+					mBackBtn,
+					"کد وارد شده صحیح نمی‌باشد"
+			);
+
+			return;
+		}
+		apiRequest.login(confirmationCode, DeviceInfo.getDeviceID(context));
+
+		progressView.setVisibility(View.VISIBLE);
+		login_get_verification_tv.setVisibility(View.INVISIBLE);
+
+		secondBtnLay.setOnClickListener(notRecievedCode);
+	}
 	private void setListeners() {
 		mBackBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -100,20 +123,7 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 
 				String confirmationCode = NumberTranslator.toEnglish(phoneConfirimationCodeEt.getText().toString());
 
-				if (!confirmationCode.trim().matches(regexStr)) {
-					Snackbari.showS(
-							mBackBtn,
-							"کد وارد شده صحیح نمی‌باشد"
-					);
-
-					return;
-				}
-				apiRequest.login(confirmationCode, DeviceInfo.getDeviceID(context));
-
-				progressView.setVisibility(View.VISIBLE);
-				login_get_verification_tv.setVisibility(View.INVISIBLE);
-
-				secondBtnLay.setOnClickListener(notRecievedCode);
+				loginWithCode(confirmationCode);
 
 			}
 		};
@@ -210,6 +220,10 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 		init();
 
 		setListeners();
+
+		IntentFilter filter = new IntentFilter();
+		filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+		registerReceiver(receiver, filter);
 	}
 
 
@@ -258,7 +272,7 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 					Constants.USER_ID,
 					tokenOutput.userId);
 
-			Toasti.showS(
+			showS(
 					"شما با شماره تلفن " +
 							NumberTranslator.toPersian(AppController.getStoredString(Constants.TELEPHONE)) +
 							" وارد شدید."
@@ -289,7 +303,57 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 
 	@Override
 	public void onFailure(Call call, Throwable t) {
+
+		if (t.getMessage().equals("incorrect_user_pass")){
+//			.showS("کد تایید اشتباه است");
+			View view = this.getCurrentFocus();
+			if (view != null) {
+				InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+			}
+
+			getWindow().setSoftInputMode(
+					WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+			);
+
+			Snackbari.showL(mToolbar, "کد تایید اشتباه است");
+		}
 		progressView.setVisibility(View.INVISIBLE);
 		login_get_verification_tv.setVisibility(View.VISIBLE);
 	}
+
+	private BroadcastReceiver receiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+//			Toast.makeText(getApplicationContext(), "received", Toast.LENGTH_SHORT).show();
+
+			if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+				Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+				SmsMessage[] msgs = null;
+				String msg_from;
+				if (bundle != null){
+					//---retrieve the SMS message received---
+					try{
+						Object[] pdus = (Object[]) bundle.get("pdus");
+						msgs = new SmsMessage[pdus.length];
+						for(int i=0; i<msgs.length; i++){
+							msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+							msg_from = msgs[i].getOriginatingAddress();
+							String msgBody = msgs[i].getMessageBody();
+							Log.d("", "onReceive: " + msg_from);
+							Log.d("", "onReceive: " + msgBody);
+
+							if (msg_from.equals("+9810002785626587")){
+								Log.d("", "onReceive: " + msgBody);
+								loginWithCode(msgBody);
+							}
+						}
+					}catch(Exception e){
+//                            Log.d("Exception caught",e.getMessage());
+					}
+				}
+			}
+
+		}
+	};
 }
