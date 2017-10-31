d5a15787027c62714a5b742ef3dfb4b0a6ae667d
Hamed.Gh <h.ghadirian@outlook.com>
2017-10-31 22:54:03 +0330

    read sms correctly

--- app/src/main/java/ir/kindnesswall/activity/LoginActivity.java
+++ app/src/main/java/ir/kindnesswall/activity/LoginActivity.java
@@ -97,14 +97,14 @@ public class LoginActivity extends AppCompatActivity implements ApiRequest.Liste
 							Log.d("", "onReceive: " + msg_from);
 							Log.d("", "onReceive: " + msgBody);
 
-//							if (AppController.getStoredString(Constants.SMS_CENTER)!=null){
-//								if (msg_from.equals(AppController.getStoredString(Constants.SMS_CENTER))) {
-								if (msg_from.equals("030002189")) {
+							if (AppController.getStoredString(Constants.SMS_CENTER)!=null){
+								if (msg_from.equals(AppController.getStoredString(Constants.SMS_CENTER))) {
+//								if (msg_from.equals("030002189")) {
 									Log.d("", "onReceive: " + msgBody);
-									msgBody.substring(msgBody.indexOf("["), msgBody.indexOf("]"));
-									loginWithCode(msgBody);
+//									msgBody.substring(msgBody.length()-6);
+									loginWithCode(msgBody.substring(msgBody.length()-6));
 								}
-//							}
+							}
 
 						}
 					} catch (Exception e) {
